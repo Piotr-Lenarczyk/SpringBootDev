f48e5b5d6b4539f6d483da19cc76c5b5735a2276
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-02-19 11:39:01 +0100

    Introductory endpoint testing

--- src/main/java/com/example/SpringBootDev/rest/MainRestController.java
+++ src/main/java/com/example/SpringBootDev/rest/MainRestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class MainRestController {
     @Value("${properties.test}")
@@ -12,5 +14,20 @@ public class MainRestController {
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Working out..";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        Random random = new Random();
+        int newInt = random.nextInt(2);
+        if (newInt == 1) {
+            return "Today is your lucky day";
+        }
+        return "Today is not lucky";
     }
 }
