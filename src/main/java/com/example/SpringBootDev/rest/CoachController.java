7768e84c5bfb89d8c32a8cbef205bd6ce53ca54b
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-02-22 10:26:13 +0100

    Qualifiers

--- src/main/java/com/example/SpringBootDev/rest/CoachController.java
+++ src/main/java/com/example/SpringBootDev/rest/CoachController.java
@@ -2,15 +2,16 @@ package com.example.SpringBootDev.rest;
 
 import com.example.SpringBootDev.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoachController {
-    private Coach coach;
+    private final Coach coach;
 
     @Autowired
-    public void setCoach(Coach coach) {
+    public CoachController(@Qualifier("baseballCoach") Coach coach) {
         this.coach = coach;
     }
 
