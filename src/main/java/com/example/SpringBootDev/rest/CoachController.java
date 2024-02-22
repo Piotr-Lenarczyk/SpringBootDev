fa7af52b4e02a2ca3d1d5215937dfd23dcc15b22
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-02-22 11:22:09 +0100

    Prototype bean scope

--- src/main/java/com/example/SpringBootDev/rest/CoachController.java
+++ src/main/java/com/example/SpringBootDev/rest/CoachController.java
@@ -9,15 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoachController {
     private final Coach coach;
+    private final Coach anotherCoach;
 
     @Autowired
-    public CoachController(@Qualifier("cricketCoach") Coach coach) {
+    public CoachController(@Qualifier("cricketCoach") Coach coach, @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("Constructing " + getClass().getSimpleName());
         this.coach = coach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return this.coach.getDailyWorkout();
+    }
+
+    @GetMapping("/checkscope")
+    public String checkScope() {
+        return "Comparing beans: myCoach == anotherCoach, " + (this.coach == this.anotherCoach);
     }
 }
