770a9298ee7cd0d4ad1ed67ab77a14d68deb93df
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-02-23 11:40:38 +0100

    Setup Hibernate

--- src/main/java/com/example/SpringBootDev/SpringBootDevApplication.java
+++ src/main/java/com/example/SpringBootDev/SpringBootDevApplication.java
@@ -1,7 +1,9 @@
 package com.example.SpringBootDev;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootDevApplication {
@@ -10,4 +12,10 @@ public class SpringBootDevApplication {
 		SpringApplication.run(SpringBootDevApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
