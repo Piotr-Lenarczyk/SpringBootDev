547aa57bb1bf428ea1489eb98f950c5099ef0099
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-02-28 09:21:36 +0100

    Path variables

--- src/main/java/com/example/SpringBootDev/rest/StudentController.java
+++ src/main/java/com/example/SpringBootDev/rest/StudentController.java
@@ -3,6 +3,7 @@ package com.example.SpringBootDev.rest;
 import com.example.SpringBootDev.entity.Student;
 import jakarta.persistence.EntityManager;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents() {
         return this.entityManager.createQuery("FROM Student", Student.class).getResultList();
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return this.entityManager.find(Student.class, studentId);
     }
 }
