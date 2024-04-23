59e39049bbfbe2a227548a89d09931a22ec11fad
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-04-23 13:49:13 +0200

    Spring Security + basic auth

--- src/main/java/com/example/SpringBootDev/dao/StudentRepository.java
+++ src/main/java/com/example/SpringBootDev/dao/StudentRepository.java
@@ -3,5 +3,6 @@ package com.example.SpringBootDev.dao;
 import com.example.SpringBootDev.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members") // Changes default endpoint path
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
