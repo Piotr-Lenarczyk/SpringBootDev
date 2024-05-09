2c8d86a5fbb1f236ea43308705df6f67e2f803dc
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-05-09 09:32:25 +0200

    Restrict URLs based on roles

--- src/main/java/com/example/SpringBootDev/security/SecurityConfig.java
+++ src/main/java/com/example/SpringBootDev/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.example.SpringBootDev.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,10 +17,30 @@ public class SecurityConfig {
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
 
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE, MANAGER").build();
+        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
 
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE, MANAGER, ADMIN").build();
+        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/students").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/students/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/students").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/students").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/students/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/students/**").hasRole("ADMIN"));
+
+        //Use Basic Auth
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //Disable Cross Site Request Forgery
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
     }
 }
