5cfb0129636bac523c479116e0a86243325cfe82
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-05-14 11:46:58 +0200

    HTML Forms

--- src/main/java/com/example/SpringBootDev/rest/TestController.java
+++ src/main/java/com/example/SpringBootDev/rest/TestController.java
@@ -1,8 +1,10 @@
 package com.example.SpringBootDev.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +13,24 @@ public class TestController {
     @GetMapping("/hello")
     public String returnHello(Model model) {
         model.addAttribute("currentTime", LocalDateTime.now());
+        return "helloworld";
+    }
+
+    @RequestMapping("/displayForm")
+    public String displayForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String capitalize(HttpServletRequest request, Model model) {
+        String message = request.getParameter("studentName");
+        message = message.toUpperCase();
+        model.addAttribute("message", message);
         return "helloworld";
     }
 }
