e10cc1f4f0c2a6b9ba7885a7b5052da48c46afd7
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-05-27 12:39:45 +0200

    @InitBinder

--- src/main/java/com/example/SpringBootDev/controller/CustomerController.java
+++ src/main/java/com/example/SpringBootDev/controller/CustomerController.java
@@ -2,10 +2,13 @@ package com.example.SpringBootDev.controller;
 
 import com.example.SpringBootDev.entity.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -27,5 +30,12 @@ public class CustomerController {
             System.out.println(customer);
             return "customer-confirmation";
         }
+    }
+
+    //InitBinder - uses StringTrimmerEditor to trim strings
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
