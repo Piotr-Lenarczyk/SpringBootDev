b007d05366421b1fd86e88ceb1f99fd688ddd925
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-06-12 11:47:10 +0200

    JDBC Authentication

--- src/main/java/com/example/SpringBootDev/security/SecurityConfiguration.java
+++ src/main/java/com/example/SpringBootDev/security/SecurityConfiguration.java
@@ -4,22 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager() {
+    //     UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
+    //     UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+    //     UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+    //
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
+    // }
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    // Uses JDBC authentication for Spring Security
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
