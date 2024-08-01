3a1b9caaf1ebbc5c71d3bece7e2d4f1b123ddff7
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-08-01 10:59:18 +0200

    JOIN FETCH

--- src/main/java/com/example/SpringBootDev/dao/CustomInstructorRepository.java
+++ src/main/java/com/example/SpringBootDev/dao/CustomInstructorRepository.java
@@ -1,9 +1,12 @@
 package com.example.SpringBootDev.dao;
 
 import com.example.SpringBootDev.entity.Course;
+import com.example.SpringBootDev.entity.Instructor;
 
 import java.util.List;
 
 public interface CustomInstructorRepository {
     List<Course> findCoursesByInstructorId(int id);
+
+    Instructor findInstructorByIdJoinFetch(int id);
 }
--- src/main/java/com/example/SpringBootDev/dao/CustomInstructorRepositoryImpl.java
+++ src/main/java/com/example/SpringBootDev/dao/CustomInstructorRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.example.SpringBootDev.dao;
 
 import com.example.SpringBootDev.entity.Course;
+import com.example.SpringBootDev.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,13 @@ public class CustomInstructorRepositoryImpl implements CustomInstructorRepositor
         TypedQuery<Course> query = this.entityManager.createQuery("FROM Course WHERE instructor.id = :data", Course.class);
         query.setParameter("data", id);
         return query.getResultList();
+    }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int id) {
+        TypedQuery<Instructor> query = this.entityManager.createQuery("SELECT i FROM Instructor i " +
+                "JOIN FETCH i.courses JOIN FETCH i.instructorDetail WHERE i.id = :data", Instructor.class);
+        query.setParameter("data", id);
+        return query.getSingleResult();
     }
 }
