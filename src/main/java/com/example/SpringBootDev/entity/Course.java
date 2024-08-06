d4a6ba0a11663ec65be8cc1f172da90b19aec49f
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2024-08-06 12:48:36 +0200

    @ManyToMany

--- src/main/java/com/example/SpringBootDev/entity/Course.java
+++ src/main/java/com/example/SpringBootDev/entity/Course.java
@@ -25,6 +25,10 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
     public Course() {
     }
 
@@ -64,11 +68,26 @@ public class Course {
         this.reviews = reviews;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     public void addReview(Review review) {
         if (this.reviews == null) {
             this.reviews = new ArrayList<>();
         }
         this.reviews.add(review);
+    }
+
+    public void addStudent(Student student) {
+        if (this.students == null) {
+            this.students = new ArrayList<>();
+        }
+        this.students.add(student);
     }
 
     @Override
@@ -78,6 +97,7 @@ public class Course {
                 ", title='" + title + '\'' +
                 ", instructor=" + instructor +
                 ", reviews=" + reviews +
+                ", students=" + students +
                 '}';
     }
 }
--- src/main/java/com/example/SpringBootDev/entity/Student.java
+++ src/main/java/com/example/SpringBootDev/entity/Student.java
@@ -2,6 +2,7 @@ package com.example.SpringBootDev.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -29,6 +30,10 @@ public class Student {
 
     @Column(name = "operating_systems")
     private List<String> operatingSystems;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses;
 
     public Student() {
     }
@@ -98,16 +103,32 @@ public class Student {
         this.operatingSystems = operatingSystems;
     }
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course) {
+        if (this.courses == null) {
+            this.courses = new ArrayList<>();
+        }
+        this.courses.add(course);
+    }
+
     @Override
     public String toString() {
         return "Student{" +
-                "id=" + this.id +
-                ", firstName='" + this.firstName + '\'' +
-                ", lastName='" + this.lastName + '\'' +
-                ", email='" + this.email + '\'' +
-                ", country='" + this.country + '\'' +
-                ", programmingLanguage='" + this.programmingLanguage + '\'' +
-                ", operatingSystems='" + this.operatingSystems + '\'' +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", programmingLanguage='" + programmingLanguage + '\'' +
+                ", operatingSystems=" + operatingSystems +
+                ", courses=" + courses +
                 '}';
     }
 }
